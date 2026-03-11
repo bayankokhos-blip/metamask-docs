00828852779020eb4f42bddd2af7388391a89267
m4sterbunny <harrie.bickle-ext@consensys.net>
2026-03-11 12:45:39 +0000

    aligns flutter with react style

--- src/pages/quickstart/builder/embedded-wallets/flutter/stepContent/index.ts
+++ src/pages/quickstart/builder/embedded-wallets/flutter/stepContent/index.ts
@@ -1,11 +1,10 @@
 import { toSteps } from '../../../../utils'
 import * as flutterQuickStart from './flutterQuickStart.mdx'
-import * as requirementsAndroid from './requirementsAndroid.mdx'
-import * as requirementsIOS from './requirementsIOS.mdx'
 import * as installation from './installation.mdx'
 import * as allowlist from './allowlist.mdx'
 import * as registerApp from '../../../../commonSteps/registerApp.mdx'
 import * as initialization from './initialize.mdx'
+import * as runTheApp from './runTheApp.mdx'
 import * as login from './login.mdx'
 import * as getUserInfo from '../../../../commonSteps/getUserInfo.mdx'
 import * as blockchainCalls from './blockchainCalls.mdx'
@@ -13,12 +12,11 @@ import * as logout from './logout.mdx'
 
 const STEPS = toSteps({
   flutterQuickStart,
-  requirementsAndroid,
-  requirementsIOS,
   installation,
   registerApp,
   allowlist,
   initialization,
+  runTheApp,
   login,
   getUserInfo,
   blockchainCalls,
--- src/pages/quickstart/builder/embedded-wallets/flutter/steps.ts
+++ src/pages/quickstart/builder/embedded-wallets/flutter/steps.ts
@@ -4,6 +4,14 @@ import STEPS from './stepContent'
 export default function getSteps(steps, files, replacementAggregator) {
   steps.push(
     {
+      ...STEPS.registerApp,
+      pointer: replacementAggregator.highlightRange(
+        qsFileLinks.EW_PNP_FLUTTER_MAIN_DART,
+        files[qsFileLinks.EW_PNP_FLUTTER_MAIN_DART],
+        'Get your Web3Auth Client ID from Dashboard'
+      ),
+    },
+    {
       ...STEPS.flutterQuickStart,
       pointer: replacementAggregator.highlightRange(
         qsFileLinks.EW_PNP_FLUTTER_MAIN_DART,
@@ -12,35 +20,11 @@ export default function getSteps(steps, files, replacementAggregator) {
       ),
     },
     {
-      ...STEPS.requirementsAndroid,
-      pointer: replacementAggregator.highlightRange(
-        qsFileLinks.EW_PNP_FLUTTER_BUILD_GRADLE,
-        files[qsFileLinks.EW_PNP_FLUTTER_BUILD_GRADLE],
-        'Requirements Android'
-      ),
-    },
-    {
-      ...STEPS.requirementsIOS,
-      pointer: replacementAggregator.highlightRange(
-        qsFileLinks.EW_PNP_FLUTTER_PODFILE,
-        files[qsFileLinks.EW_PNP_FLUTTER_PODFILE],
-        'Requirements iOS'
-      ),
-    },
-    {
       ...STEPS.installation,
       pointer: replacementAggregator.highlightRange(
         qsFileLinks.EW_PNP_FLUTTER_PUBSPEC_YAML,
         files[qsFileLinks.EW_PNP_FLUTTER_PUBSPEC_YAML],
         'Installation'
-      ),
-    },
-    {
-      ...STEPS.registerApp,
-      pointer: replacementAggregator.highlightRange(
-        qsFileLinks.EW_PNP_FLUTTER_MAIN_DART,
-        files[qsFileLinks.EW_PNP_FLUTTER_MAIN_DART],
-        'Get your Web3Auth Client ID from Dashboard'
       ),
     },
     {
@@ -57,6 +41,14 @@ export default function getSteps(steps, files, replacementAggregator) {
         qsFileLinks.EW_PNP_FLUTTER_MAIN_DART,
         files[qsFileLinks.EW_PNP_FLUTTER_MAIN_DART],
         'Initialize Web3Auth'
+      ),
+    },
+    {
+      ...STEPS.runTheApp,
+      pointer: replacementAggregator.highlightRange(
+        qsFileLinks.EW_PNP_FLUTTER_MAIN_DART,
+        files[qsFileLinks.EW_PNP_FLUTTER_MAIN_DART],
+        'Quick Start'
       ),
     },
     {
